1d870b04e52ddbca62f438cc6002f197be7069a3
Matthew Di Micelli <matthewdimicelli@gmail.com>
2024-12-28 19:06:13 -0500

    work on 4.2.  express app code lives in app.js

--- index.js
+++ index.js
@@ -1,33 +1,4 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-mongoose.connect(process.env.MONGODB_URI);
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/blogs', async (req, res) => {
-  const blogs = await Blog.find({});
-  res.json(blogs);
-})
-
-app.post('/api/blogs', async (req, res) => {
-  const blog = new Blog(req.body);
-  const result = await blog.save();
-  res.status(201).json(result);
-})
+const app = require('./app');
 
 const PORT = 3003
 app.listen(PORT, () => {
