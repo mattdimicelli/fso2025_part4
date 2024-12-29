09483b82d218a2e458eb5a8202dbb7bd2da1fe81
Matthew Di Micelli <matthewdimicelli@gmail.com>
2024-12-29 13:36:23 -0500

    finish 4.8

---
+++ tests/getAllBlogs.test.js
@@ -0,0 +1,68 @@
+const app = require('../app');
+const supertest = require('supertest');
+const { test, describe, after, beforeEach, before } = require('node:test');
+const mongoose = require('mongoose');
+const Blog = require('../models/blog');
+const assert = require('assert');
+
+const blogs = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+  },
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+  },
+  {
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+  },
+  {
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+  },
+  {
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+  }
+]
+beforeEach(async () => {
+  await Blog.deleteMany({});
+})
+test('the return format is json', async() => {
+  await supertest(app).get('/api/blogs').expect('Content-Type', /application\/json/);
+});
+
+describe('the number of blogs returned matches the number of blogs in the db', () => {
+  test('return three blogs', async () => {
+    const threeBlogs = blogs.slice(0, 3);
+    for (let i = 0; i < threeBlogs.length; i++) {
+      const blog = new Blog(threeBlogs[i]);
+      await blog.save();
+    }
+    const response = await supertest(app).get('/api/blogs');
+    assert.strictEqual(response.body.length, 3);
+  });
+});
+
+after(async () => {
+  await mongoose.connection.close();
+})
+
--- utilities/config.js
+++ utilities/config.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const PORT = 3003;
-const mongoDbUri = process.env.MONGODB_URI
+const mongoDbUri = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI;
 
 module.exports = {
   PORT, mongoDbUri,
