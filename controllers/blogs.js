a5ac1dd70037d940f69801ca1690571bc620dbbd
PA_MDiMicelli <mdimicelli@northwell.edu>
2025-01-13 18:54:38 -0500

    finish 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,17 +1,39 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 blogRouter.get('/api/blogs', async (req, res) => {
   const blogs = await Blog.find({}).populate('user');
   res.json(blogs);
 });
 
 blogRouter.post('/api/blogs', async (req, res) => {
-  const randomUser = await User.findOne({});
-  const blog = new Blog({...req.body, user: randomUser.id });
-  const result = await blog.save();
-  const populatedBlog = await Blog.findById(result.id).populate('user');
-  res.status(201).json(populatedBlog);
+  try {
+    const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET);
+    if (!decodedToken.id) {
+      return res.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id);
+    const blog = new Blog({...req.body, user: user.id });
+    const result = await blog.save();
+    const populatedBlog = await Blog.findById(result.id).populate('user');
+    res.status(201).json(populatedBlog);
+  } catch(e) {
+    if (e.name === 'JsonWebTokenError' && (e.message === 'jwt must be provided' || e.message === 'jwt malformed')) {
+      return res.status(401).json({ error: 'token invalid' })
+    } else {
+      console.log(e);
+      return;
+    }
+  }
 });
 
 blogRouter.patch('/api/blogs/:id', async (req, res) => {
--- tests/postBlog.test.js
+++ tests/postBlog.test.js
@@ -5,6 +5,8 @@ const app = require('../app');
 const Blog = require('../models/blog');
 const mongoose = require('mongoose');
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 const blogs = [
   {
@@ -47,9 +49,10 @@ const blogs = [
 
 beforeEach(async () => {
   await Blog.deleteMany({});
+  await User.deleteMany({});
 });
 
-describe('posting a blog', () => {
+describe('posting a blog along with a valid token', () => {
   test('... will increase the blog count by one', async() => {
     for (let i = 0; i < blogs.length; i++) {
       const blog = new Blog(blogs[i]);
@@ -62,7 +65,27 @@ describe('posting a blog', () => {
       url: "http://something.com",
       likes: 7,
     };
-    await supertest(app).post('/api/blogs').send(JSON.stringify(newBlog)).set('Content-Type', 'application/json');
+
+    const userPw = 'thisisthepassword';
+    const saltRounds = 10;
+    const hash = await bcrypt.hash(userPw, saltRounds);
+
+    let userToLogIn = {
+        username: 'mdimicelli',
+        name: 'Matt Di Micelli',
+        hash: hash,
+    }
+    userToLogIn = new User(userToLogIn);
+    await userToLogIn.save();
+
+    const token = jwt.sign({ username: 'mdimicelli', id: userToLogIn.id }, process.env.SECRET);
+
+    await supertest(app)
+      .post('/api/blogs')
+      .send(JSON.stringify(newBlog))
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`);
+
     const response = await supertest(app).get('/api/blogs');
     const dbBlogs = response.body;
     assert.strictEqual(7, dbBlogs.length);
@@ -75,7 +98,26 @@ describe('posting a blog', () => {
       url: "http://something.com",
       likes: 7,
     };
-    await supertest(app).post('/api/blogs').send(JSON.stringify(newBlog)).set('Content-Type', 'application/json');
+
+    const userPw = 'thisisthepassword';
+    const saltRounds = 10;
+    const hash = await bcrypt.hash(userPw, saltRounds);
+
+    let userToLogIn = {
+      username: 'mdimicelli',
+      name: 'Matt Di Micelli',
+      hash: hash,
+    }
+    userToLogIn = new User(userToLogIn);
+    await userToLogIn.save();
+
+    const token = jwt.sign({ username: 'mdimicelli', id: userToLogIn.id }, process.env.SECRET);
+
+    await supertest(app)
+      .post('/api/blogs')
+      .send(JSON.stringify(newBlog))
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`);
     const response = await supertest(app).get('/api/blogs');
     const dbNewBlog = response.body[0];
     assert.strictEqual(dbNewBlog.title, newBlog.title);
@@ -84,39 +126,96 @@ describe('posting a blog', () => {
     assert.strictEqual(dbNewBlog.likes, newBlog.likes);
   });
 
-  test('... will cause any user from the db to be designated as its creator', async () => {
-    let user1 = {
-      name: 'Matt Di Micelli',
-      hash: 'ooglyboogly',
-      username: 'mdimicelli',
-    };
-    let user2 = {
-      name: 'Meri',
-      hash: 'ooglyboogly',
-      username: 'santaella',
-    };
-    user1 = new User(user1);
-    user2 = new User(user2);
-    await user1.save();
-    await user2.save();
+  test('... will make the user identified by the token as the creator of the blog', async () => {
+      const newBlog = {
+        title: "New blog",
+        author: "Matt Di Micelli",
+        url: "http://something.com",
+        likes: 7,
+      };
+      const userPw = 'thisisthepassword';
+      const saltRounds = 10;
+      const hash = await bcrypt.hash(userPw, saltRounds);
 
-    const newBlog = {
-      title: "New blog",
-      author: "Matt Di Micelli",
-      url: "http://something.com",
-      likes: 7,
-    };
-    await supertest(app)
-      .post('/api/blogs')
-      .send(JSON.stringify(newBlog))
-      .set('Content-Type', 'application/json')
-      .expect(201)
-      .expect((res) => {
-        assert.ok(res.body.user);
-    })
-  })
+      let userToLogIn = {
+        username: 'mdimicelli',
+        name: 'Matt Di Micelli',
+        hash: hash,
+      }
+      userToLogIn = new User(userToLogIn);
+      await userToLogIn.save();
+
+      const token = jwt.sign({ username: 'mdimicelli', id: userToLogIn.id }, process.env.SECRET);
+
+      await supertest(app)
+        .post('/api/blogs')
+        .send(JSON.stringify(newBlog))
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${token}`);
+
+      const response = await supertest(app).get('/api/blogs');
+      const dbBlogs = response.body;
+      assert.strictEqual(dbBlogs[0].user.name, 'Matt Di Micelli');
+      assert.strictEqual(dbBlogs[0].user.username, 'mdimicelli');
+  });
+
+//   test('... will cause any user from the db to be designated as its creator', async () => {
+//     let user1 = {
+//       name: 'Matt Di Micelli',
+//       hash: 'ooglyboogly',
+//       username: 'mdimicelli',
+//     };
+//     let user2 = {
+//       name: 'Meri',
+//       hash: 'ooglyboogly',
+//       username: 'santaella',
+//     };
+//     user1 = new User(user1);
+//     user2 = new User(user2);
+//     await user1.save();
+//     await user2.save();
+//
+//     const newBlog = {
+//       title: "New blog",
+//       author: "Matt Di Micelli",
+//       url: "http://something.com",
+//       likes: 7,
+//     };
+//     await supertest(app)
+//       .post('/api/blogs')
+//       .send(JSON.stringify(newBlog))
+//       .set('Content-Type', 'application/json')
+//       .expect(201)
+//       .expect((res) => {
+//         assert.ok(res.body.user);
+//     })
+//   })
+
+
+  describe('posting a blog', () => {
+    test('... without a token will result in a 401 - Unauthorized response', async () => {
+      const blog = blogs[0];
+
+      await supertest(app)
+        .post('/api/blogs')
+        .send(JSON.stringify(blog))
+        .set('Content-Type', 'application/json')
+        .set('Authorization', 'Bearer ')
+        .expect(401);
+    });
+
+    test('... with an invalid token will result in a 401 - Unauthorized response', async () => {
+      const blog = blogs[0];
+
+      await supertest(app)
+        .post('/api/blogs')
+        .send(JSON.stringify(blog))
+        .set('Content-Type', 'application/json')
+        .set('Authorization', 'Bearer boogedywoogedy')
+        .expect(401);
+    });
+  });
 });
-
 
 after(async () => {
   await mongoose.connection.close();
